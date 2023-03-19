621a54aeeb911ba22b607f8e356b7801485332da
vargasdm <112888008+vargasdm@users.noreply.github.com>
2023-03-19 16:03:06 -0400

    fixed image rendering in saved books page

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -41,7 +41,7 @@ const resolvers = {
                 return User.findOneAndUpdate(
                     { _id: context.user._id },
                     {
-                        $addToSet: { savedBooks: { bookData } },
+                        $push: { savedBooks: bookData },
                     },
                     {
                         new: true,
--- server/schemas/typeDefs.js
+++ server/schemas/typeDefs.js
@@ -20,7 +20,7 @@ const typeDefs = gql`
   }
 
   input SearchBookInput {
-    authors: String
+    authors: [String]
     description: String
     bookId: String
     image: String
@@ -40,7 +40,7 @@ const typeDefs = gql`
   type Mutation {
     addUser(username: String!, email: String!, password: String!): Auth
     login(email: String!, password: String!): Auth
-    saveBook(bookData: [SearchBookInput!]): User
+    saveBook(bookData: SearchBookInput!): User
     removeBook(bookId: ID!): User
   }
 `;
