2b498f082f61c0edb1fe1e6ab062625f9922daff
vargasdm <112888008+vargasdm@users.noreply.github.com>
2023-03-18 21:30:56 -0400

    imported signtoken and authtificationerror in mutations.js

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -1,4 +1,7 @@
 const { Book, User } = require('../models');
+const { AuthenticationError } = require('apollo-server-express');
+const { signToken } = require('../utils/auth');
+
 
 const resolvers = {
     Query: {
